a98a949f00280f3417216a12f5a322fa85be6876
igorkishko <kishko.igor@gmail.com>
2022-02-02 17:24:38 +0200

    Layout component

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router';
-import Layout from './components/Layout';
 import Home from './pages/Home';
 
 const Main = () => (
-  <Layout>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Redirect to="/" />
-    </Switch>
-  </Layout>
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Redirect to="/" />
+  </Switch>
 );
 
 export default Main;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Header from './Header';
-import Footer from './Footer';
+import { Link } from 'react-router-dom';
+import { Header } from './Header';
+import { Footer } from './Footer';
+import ClockIcon from '../assets/svg/layout__clock-icon.svg';
 
-const Layout = (props) => {
-  const { children } = props;
-  return (
-    <div className="layout">
-      <Header />
-      <div className="content">
-        <div className="page">{children}</div>
+import '../styles/Main.scss';
+
+export const Layout = ({ children, title }) => (
+  <>
+    <Header />
+    <main className="layout">
+      <div className="content layout__heading-container">
+        <h1 className="layout__heading">{title}</h1>
+        <a href="/" className="button button_gray layout__button">
+          Ambrosus Bridge Guide
+        </a>
+        <Link to="/" className="button button_black layout__button">
+          <img
+            src={ClockIcon}
+            alt="clock icon"
+            className="layout__button-icon"
+          />
+          Transaction history
+        </Link>
       </div>
-      <Footer />
-    </div>
-  );
-};
+      <div className="layout__container">{children}</div>
+    </main>
+
+    <Footer />
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.element,
+  title: PropTypes.string,
 };
 
 export default Layout;
