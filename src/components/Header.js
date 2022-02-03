f78fa092296b5a2fbb588dc1369035ecb14fb8e6
igorkishko <kishko.igor@gmail.com>
2022-02-03 15:35:30 +0200

    Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,81 @@
-import React from 'react';
-const Header = () => <h1>Header</h1>;
+import * as React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/svg/logo.svg';
 
-export default Header;
+export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <header className="header">
+        <div className="content header__content">
+          <Link to="/" className="header__logo-wrapper">
+            <img src={Logo} alt="logo" className="header__logo" />
+          </Link>
+
+          <a href="https://ambrosus.io/about/" className="header__link">
+            About Ambrosus
+          </a>
+          <a href="https://ambrosus.io/business/" className="header__link">
+            Business
+          </a>
+          <a href="https://ambrosus.io/developers" className="header__link">
+            Developers
+          </a>
+          <a href="https://ambrosus.io/community" className="header__link">
+            Community
+          </a>
+
+          <div
+            className={`burger-icon ${isOpen ? 'burger-icon_open' : ''}`}
+            onClick={toggleMenu}
+            role="button"
+            aria-hidden
+          >
+            <span className="burger-icon__first-line burger-icon__line" />
+            <span className="burger-icon__second-line burger-icon__line" />
+            <span className="burger-icon__third-line burger-icon__line" />
+          </div>
+        </div>
+      </header>
+
+      <div className={`mobile-menu ${isOpen ? 'mobile-menu_open' : ''}`}>
+        <a
+          href="https://ambrosus.io/about/"
+          className="mobile-menu__link"
+          data-number={`—\u00A00${1}`}
+          onClick={toggleMenu}
+        >
+          About Ambrosus
+        </a>
+        <a
+          href="https://ambrosus.io/business/"
+          className="mobile-menu__link"
+          data-number={`—\u00A00${2}`}
+          onClick={toggleMenu}
+        >
+          Business
+        </a>
+        <a
+          href="https://ambrosus.io/developers"
+          className="mobile-menu__link"
+          data-number={`—\u00A00${3}`}
+          onClick={toggleMenu}
+        >
+          Developers
+        </a>
+        <a
+          href="https://ambrosus.io/community"
+          className="mobile-menu__link"
+          data-number={`—\u00A00${4}`}
+          onClick={toggleMenu}
+        >
+          Community
+        </a>
+      </div>
+    </>
+  );
+};
