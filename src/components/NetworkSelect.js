2b6889bcfa3ddc4a8f8b3b4f5662b8e2dec72794
igorkishko <kishko.igor@gmail.com>
2022-02-23 23:01:02 +0200

    Network select component logic

--- src/components/NetworkSelect.js
+++ src/components/NetworkSelect.js
@@ -2,43 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ChevronIcon from '../assets/svg/chevron.svg';
 
-const NetworkSelect = ({ disabled = false }) => (
-  <div
-    className={`network-select ${disabled ? 'network-select_disabled' : ''}`}
-  >
+const NetworkSelect = ({
+  networks = [{}],
+  setChainId = () => {},
+  selectedChainId = 0,
+}) => {
+  // this sort is setting selected network first in the list,
+  // so it can be visible
+  const sortedNetworks = networks.sort((a) =>
+    a.chainId === selectedChainId ? -1 : 1,
+  );
+
+  return (
     <div
-      className="network-select__absolute-wrapper"
-      style={{ '--items-amount': 2 }} // 2 is mock const, it will be amount of networks
+      className={`network-select ${
+        networks.length === 1 ? 'network-select_disabled' : ''
+      }`}
     >
-      <div className="network-select__option">
-        {/* mock image */}
-        <img
-          src="https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/WETH/logo.png"
-          alt="#"
-          className="network-select__currency-icon"
-        />
-        Ethereum
+      <div
+        className="network-select__absolute-wrapper"
+        style={{ '--items-amount': networks.length }}
+      >
+        {sortedNetworks.map(({ name = '', chainId }) => (
+          <button
+            type="button"
+            className="network-select__option"
+            key={`select-option-${chainId}`}
+            onClick={() => setChainId(chainId)}
+          >
+            {/* mock image */}
+            <img
+              src="https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/WETH/logo.png"
+              alt="#"
+              className="network-select__currency-icon"
+            />
+            {name}
+          </button>
+        ))}
       </div>
-      <div className="network-select__option">
-        {/* mock image */}
-        <img
-          src="https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/WETH/logo.png"
-          alt="#"
-          className="network-select__currency-icon"
-        />
-        Binance Smart Chain
-      </div>
+      <img
+        src={ChevronIcon}
+        alt="chevron icon"
+        className="network-select__chevron"
+      />
     </div>
-    <img
-      src={ChevronIcon}
-      alt="chevron icon"
-      className="network-select__chevron"
-    />
-  </div>
-);
+  );
+};
 
 NetworkSelect.propTypes = {
-  disabled: PropTypes.bool,
+  networks: PropTypes.arrayOf(PropTypes.object),
+  setChainId: PropTypes.func,
+  selectedChainId: PropTypes.number,
 };
 
 export default NetworkSelect;
