403f6b2682092e5684834da05dd8627aee9d5f66
igorkishko <kishko.igor@gmail.com>
2022-05-05 15:08:29 +0300

    Settled longpoll provider for ambrosus

--- src/utils/providers.js
+++ src/utils/providers.js
@@ -1,7 +1,11 @@
 import { ethers } from 'ethers';
 
-const { REACT_APP_ETH_CHAIN_ID, REACT_APP_AMB_CHAIN_ID, REACT_APP_INFURA_KEY } =
-  process.env;
+const {
+  REACT_APP_ETH_CHAIN_ID,
+  REACT_APP_AMB_CHAIN_ID,
+  REACT_APP_INFURA_KEY,
+  REACT_APP_AMB_RPC_URL,
+} = process.env;
 
 // ethereum read-only provider configuration
 export const ethChainId = +REACT_APP_ETH_CHAIN_ID;
@@ -14,8 +18,8 @@ const ethProvider = new ethers.providers.InfuraWebSocketProvider(
 // ambrosus read-only provider configuration
 export const ambChainId = +REACT_APP_AMB_CHAIN_ID;
 
-export const ambProvider = new ethers.providers.WebSocketProvider(
-  'wss://network.ambrosus-dev.io/ws',
+export const ambProvider = new ethers.providers.StaticJsonRpcProvider(
+  REACT_APP_AMB_RPC_URL,
   ambChainId,
 );
 
