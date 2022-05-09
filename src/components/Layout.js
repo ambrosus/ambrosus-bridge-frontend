ed82455c1a417f82eb1a18685110ae6e09abb9dd
igorkishko <kishko.igor@gmail.com>
2022-05-09 19:06:29 +0300

    Hide transaction history for unauthorized users

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useWeb3React } from '@web3-react/core';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { ErrorWidget } from './ErrorWidget';
@@ -11,62 +12,70 @@ import SphereIllustration from '../assets/svg/layout__sphere.svg';
 
 import '../styles/Main.scss';
 
-export const Layout = ({ children, title, error }) => (
-  <div className="root">
-    <ErrorWidget error={error} />
-    <Header />
-    <main className="layout">
-      <div className="content layout__heading-container">
-        <h1 className="layout__heading">{title}</h1>
+export const Layout = ({ children, title, error }) => {
+  const { account } = useWeb3React();
+  return (
+    <div className="root">
+      <ErrorWidget error={error} />
+      <Header />
+      <main className="layout">
+        <div className="content layout__heading-container">
+          <h1 className="layout__heading">{title}</h1>
 
-        {/* desktop buttons block start */}
-        <a href="/" className="button button_gray layout__button">
-          Ambrosus Bridge Guide
-        </a>
+          {/* desktop buttons block start */}
+          <a href="/" className="button button_gray layout__button">
+            Ambrosus Bridge Guide
+          </a>
 
-        <Link to="/history" className="button button_black layout__button">
-          <img
-            src={ClockIcon}
-            alt="clock icon"
-            className="layout__button-icon"
-          />
-          Transaction history
-        </Link>
-        {/* desktop buttons block end */}
+          {account ? (
+            <Link to="/history" className="button button_black layout__button">
+              <img
+                src={ClockIcon}
+                alt="clock icon"
+                className="layout__button-icon"
+              />
+              Transaction history
+            </Link>
+          ) : null}
 
-        {/* mobile buttons block start */}
-        <a href="/" className="layout__mobile-button">
-          <img
-            src={BookIcon}
-            alt="book icon"
-            className="layout__mobile-button-icon"
-          />
-        </a>
+          {/* desktop buttons block end */}
 
-        <Link to="/history" className="layout__mobile-button">
-          <img
-            src={ClockIcon}
-            alt="clock icon"
-            className="layout__mobile-button-icon"
-          />
-        </Link>
-        {/* mobile buttons block end */}
-      </div>
-      <div className="layout__container">{children}</div>
-    </main>
-    <Footer />
-    <img
-      src={PyramidIllustration}
-      alt="pyramid illustration"
-      className="root__pyramid"
-    />
-    <img
-      src={SphereIllustration}
-      alt="sphere illustration"
-      className="root__sphere"
-    />
-  </div>
-);
+          {/* mobile buttons block start */}
+          <a href="/" className="layout__mobile-button">
+            <img
+              src={BookIcon}
+              alt="book icon"
+              className="layout__mobile-button-icon"
+            />
+          </a>
+
+          {account ? (
+            <Link to="/history" className="layout__mobile-button">
+              <img
+                src={ClockIcon}
+                alt="clock icon"
+                className="layout__mobile-button-icon"
+              />
+            </Link>
+          ) : null}
+          {/* mobile buttons block end */}
+        </div>
+        <div className="layout__container">{children}</div>
+      </main>
+      <Footer />
+      <img
+        src={PyramidIllustration}
+        alt="pyramid illustration"
+        className="root__pyramid"
+      />
+      <img
+        src={SphereIllustration}
+        alt="sphere illustration"
+        className="root__sphere"
+      />
+    </div>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node,
