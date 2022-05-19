9a035e8de6214ffb4e0e76ab8d191ef83e50bb5f
dimavoronin <zveruha1109>
2022-05-19 16:05:55 +0300

    iphone exchange input

--- src/pages/Exchange/CurrencyInput.js
+++ src/pages/Exchange/CurrencyInput.js
@@ -55,7 +55,6 @@ const CurrencyInput = ({
         onChange={handleInput}
         onKeyPress={handleKeyPress}
         readOnly={disabled}
-        pattern="\d*"
       />
       <button
         type="button"
